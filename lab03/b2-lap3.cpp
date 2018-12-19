7f175f680823655e290fcdbbd9ca5af1aabcf998
trinhbaonhan <n17dccn117@student.ptithcm.edu.vn>
2018-12-19 23:24:47 +0700

    Update lab03/b2-lap3.cpp

---
+++ lab03/b2-lap3.cpp
@@ -0,0 +1,33 @@
+# include<iostream>
+using namespace std;
+class nhanvien
+{
+	private:
+		int id;
+		float money;	
+	public:
+		nhanvien(int id,float m)
+		{
+			this-> id=id;
+			this-> money=m;
+		}
+		void tinh(int year)
+		{
+			float m=this->money;
+			for(int i=0;i<year;i++)
+			{
+				m+=m*0.055;
+			}
+				cout<<"so tien sau "<<year<<"nam ="<<m<<endl;
+		}
+		
+};
+int main()
+{
+	nhanvien s1();
+	cout<<"nhap dia chi id cua nhan vien s1 va tien boi thuong"<<endl;
+	cin>>s1>>m;
+	//nhanvien s1(1,2500);
+	s1.tinh(3);
+}
+
